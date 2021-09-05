7b1abe41513fa25922422a6ebca53f3f8ce46b36
jp <dashjp1234@gmail.com>
2021-09-05 15:20:42 +0530

    bubblesort updated

--- BubbleSort/Bubblesort.java
+++ BubbleSort/Bubblesort.java
@@ -1,5 +1,36 @@
+import java.util.Arrays;
+
 class Bubblesort {
-    public static void main(String[] args) {
-        System.out.println("nf dnvfvfkvbfdnvdnv nv n ");
+    public static void main(String[] args)
+    {
+        int[] arr = {1,2,3,4,5};
+        bubble(arr);
+        System.out.println(Arrays.toString(arr));
+
     }
+        static void bubble(int[] arr)
+        {
+            boolean swapped ;
+            for(int  i = 0;i<arr.length;i++)
+            {
+                swapped =false;
+                for(int j =1;j<arr.length-i;j++)
+                {
+                    if(arr[j] <arr[j-1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j-1];
+                        arr[j-1] = temp;
+                     swapped = true;
+                    }
+                }
+                if(!swapped  ){
+                    break;
+                }
+            }
+
+        }
+
+
+
 }
